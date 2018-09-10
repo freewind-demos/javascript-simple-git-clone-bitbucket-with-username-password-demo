34b83e7bb436e2a1bc5d14f77cf50b4f63ba9ec9
freewind <nowindlee@gmail.com>
2018-09-10 15:53:38 +0800

    username/password demo

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-JavaScript "simple-git" Clone Demo
-==================================
+JavaScript "simple-git" Clone BitBucket With Username Password Demo
+===================================================================
 
-使用"simple-git"将指定的git仓库clone到本地
+使用"simple-git"以及username/password将指定的bitbucket仓库clone到本地
 
 ```
 npm install
-node run demo
+rm -rf local-repo
+mkdir local-repo
+node demo.js <username> <password>
 ```
+
+注意`username`不能使用邮箱（因为里面的`@`会导致url出错），而应该使用用户名。
 
 然后就会在`local-repo`目录下看到新clone下来的`demo-project`。
--- demo.js
+++ demo.js
@@ -1,7 +1,17 @@
 const workingDirPath = './local-repo'
 const simpleGit = require('simple-git')(workingDirPath)
 
-simpleGit.clone('https://github.com/freewind-demos/javascript-frontend-hello-world-demo.git', 'demo-project')
-    .exec(function () {
-        console.log('cloned to: local-repo/demo-project')
-    })
+const username = process.argv[2]
+const password = process.argv[3]
+console.log(`username: ${username}`)
+console.log(`password: ${password}`)
+
+if (username && password) {
+    const gitUrl = `https://${username}:${password}@bitbucket.org/Freewind/java-hello-world-demo.git`
+
+    simpleGit.clone(gitUrl, 'demo-project')
+        .exec(function () {
+            console.log('cloned to: local-repo/demo-project')
+        })
+}
+
